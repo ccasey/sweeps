e62314fdf64a31020648e6b2531024c4d531a41e
Chris Casey <cscvrp@gmail.com>
2023-11-18 19:30:35 -0600

    cleanup loading log

--- sections-needed.pl
+++ sections-needed.pl
@@ -104,9 +104,6 @@ while(<LL>){
    	}else{
     	$qsos{$foo[4]} = { action => "add", sserial => $foo[1], rserial => $foo[2], precedence => $foo[3], check => $foo[5],
                       	section => $foo[6], qsotime => $foo[7], freq => $foo[8]};
-      json_logit($foo[4]);
-    	$last_qso = $foo[4];
-    	$Freq = $foo[8];
    	}
  }
 
